ace6cbe45e187d9c1a6727ee3b394a916a5d1628
PRITESH SRIVASTAVA <anshu.68ec@gmail.com>
2017-03-17 02:39:22 +0530

    Whatsapp-automation
    Python code to send whatsapp messages to friend or group.

---
+++ Whatsapp_msg.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+# Replace below path with the absolute path
+# to chromedriver in your computer
+driver = webdriver.Chrome('/home/anshu/Downloads/chromedriver')
+
+driver.get("https://web.whatsapp.com/")
+wait = WebDriverWait(driver, 600)
+
+# Replace 'Friend's Name' with the name of your friend
+# or the name of a group
+target = "Friend's Name"
+
+# Replace the below string with your own message
+string = "How you doin'...? ;)"
+y_arg = '//*[@id="side"]/div[2]/div/label/input'
+input_y = wait.until(EC.presence_of_element_located((
+    By.XPATH, y_arg)))
+input_y.send_keys(target + Keys.ENTER)
+
+inp_xpath = '//*[@id="main"]/footer/div[1]/div[1]/div/div[2]'
+'//*[@id="main"]/footer/div[1]/div[1]/div/div[2]'
+input_box = wait.until(EC.presence_of_element_located((
+    By.XPATH, inp_xpath)))
+for i in range(2):
+    input_box.send_keys(string + Keys.ENTER)
+    time.sleep(1)
